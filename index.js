8460d01569dfa03eb51102c6862420374ded317d
MichaelStafford <michaelstafford808@gmail.com>
2021-09-11 19:14:51 -1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+var element = document.createElement('div');
+
+document.getElementById('main').remove();
+
+var newHeader = document.createElement('h1');
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "Michael is the champion";
+
+document.body.appendChild(newHeader);
