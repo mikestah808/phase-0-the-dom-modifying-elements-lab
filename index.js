b20504f659ec3f2076e8e172702730fb72baf9a9
Michael Stafford <michaelstafford808@gmail.com>
2022-01-12 12:27:04 -0700

    commit tree 19d25fbebeaf3a006d530ba070e21f6e222c79f7

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 // Write your code here!
-var element = document.createElement('div');
-
 document.getElementById('main').remove();
-
 var newHeader = document.createElement('h1');
 newHeader.setAttribute("id","victory");
 newHeader.innerHTML = "Michael is the champion";
-
-document.body.appendChild(newHeader);
